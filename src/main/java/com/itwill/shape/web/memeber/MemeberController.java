36a298b2f76c464b2c7648d5b8c02b5ad9d56798
HCS <HCS@DESKTOP-PLDU4F9>
2023-05-29 21:53:10 +0900

    DB 연결 테스트 및 controller 테스트

---
+++ src/main/java/com/itwill/shape/web/memeber/MemeberController.java
@@ -0,0 +1,21 @@
+package com.itwill.shape.web.memeber;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class MemeberController {
+	
+	/**
+	 * 사용자 login
+	 * @return
+	 */
+	@GetMapping("/login")
+	public void login() {
+		log.info("login()");
+	}
+	
+}
