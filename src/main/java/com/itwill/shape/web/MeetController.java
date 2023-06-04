dd9998470284b6f29f48028c50714221d321c8fb
SEC <SEC@LAPTOP-DPLRG6AO>
2023-06-05 03:11:40 +0900

    배선영controller

--- src/main/java/com/itwill/shape/web/MeetController.java
+++ src/main/java/com/itwill/shape/web/MeetController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.shape.domain.MeetPrtcp;
 import com.itwill.shape.dto.MeetInfoCreateDto;
@@ -139,4 +140,5 @@ public class MeetController {
 		return ResponseEntity.ok(list);
 		
 	}
+	
 }
