08b5eb51248e512ba3c758c9545e203dde99f4b5
drj9812 <drj9812@gmail.com>
2023-06-08 09:52:31 +0900

    손창민 커밋

--- src/main/java/com/itwill/shape/web/MyPageRestController.java
+++ src/main/java/com/itwill/shape/web/MyPageRestController.java
@@ -1,6 +1,6 @@
 package com.itwill.shape.web;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itwill.shape.dto.PostCommentDeleteDto;
+//import com.itwill.shape.dto.PostCommentDeleteDto;
 import com.itwill.shape.service.PostCommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,20 +21,20 @@ import lombok.extern.slf4j.Slf4j;
 public class MyPageRestController {
 
 	
-	@PostMapping("/deleteComments")
-	  public ResponseEntity<String> deleteComments(@RequestBody List<PostCommentDeleteDto> PostCommentDeleteDtoList) {
-	    for (PostCommentDeleteDto postCommentDeletDto : PostCommentDeleteDtoList) {
-	      String commentAuthor = postCommentDeletDto.getAuthor();
-	      String commentContent = postCommentDeletDto.getContent();
-
-	      // commentAuthor와 commentContent를 기반으로 post-comment 테이블에서 해당 댓글을 삭제하는 작업 수행
-	      PostCommentService.deleteComment(commentAuthor, commentContent);
-	    }
-
-	    return ResponseEntity.ok().build();
-	  }
-
-	}
+//	@PostMapping("/deleteComments")
+//	  public ResponseEntity<String> deleteComments(@RequestBody List<PostCommentDeleteDto> PostCommentDeleteDtoList) {
+//	    for (PostCommentDeleteDto postCommentDeletDto : PostCommentDeleteDtoList) {
+//	      String commentAuthor = postCommentDeletDto.getAuthor();
+//	      String commentContent = postCommentDeletDto.getContent();
+//
+//	      commentAuthor와 commentContent를 기반으로 post-comment 테이블에서 해당 댓글을 삭제하는 작업 수행
+//	      PostCommentService.deleteComment(commentAuthor, commentContent);
+//	    }
+//
+//	    return ResponseEntity.ok().build();
+//	  }
+//
+//	}
 
 	
 }
