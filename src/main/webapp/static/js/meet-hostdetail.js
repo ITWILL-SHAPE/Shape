f5fa637fade39a947452b579f2930905f5ff29bd
baesunyoung34 <baesaa0304@nvaer.com>
2023-06-09 17:13:59 +0900

    배선영 js jsp

--- src/main/webapp/static/js/meet-hostdetail.js
+++ src/main/webapp/static/js/meet-hostdetail.js
@@ -1,22 +1,29 @@
 /**
  * host가 수정버튼 삭제버튼 누를경우
  * 디테일 서비스
+ * 
+ *  </c:url>
+	             <a href="${ updatePage }" type="button" class="btn btn-outline-primary btn-lg" id="btnUpdate" name="btnUpdate">수정하기</a>
+	                  <a href="${delete }" type="button" class="btn btn-outline-danger btn-lg" id="btnDelete" name="btnDelete">삭제하기</a>   
+	               </div>
+	                  </div>
+	        </c:if>
  */
-document.addEventListener('DOMContentLoaded' , () =>{
-	
-	
-	// 삭제 이벤트
-	const btnDelete = document.querySelector('#btnDelete');
-	btnDelete.addEventListener('click', () =>{
-		const check = confirm('정말 삭제할까요?');
-		if(check) {
-			 
-		}
-	});
-	
-	
-	
-	
-	
-	
-});	
+ const inputmtid = document.querySelector('input#mtid');
+ const btnUpdate = document.querySelector('button#btnUpdate');
+ const btnDelete = document.querySelector('button#btnDelete');
+ 
+ btnDelete.addEventListener('click',(e) => {
+	 e.preventDefault();
+	 
+	 const id= inputmtid.value;
+	 const result = confirm(`NO.${id} 정말 삭제할까요?`);
+	 // 확인
+	 console.log(`삭제 확인 결과 = ${result}`);
+	 if(result){
+		 form.action = 'delete';
+		 form.method = '';
+		 form.submit();
+	 }
+ });
+ 
--- src/main/webapp/static/js/meet-maindetaill.js
+++ src/main/webapp/static/js/meet-maindetaill.js
@@ -1,52 +1,169 @@
-/**
- * 찜 참여완료 버튼
- * const getdetailByMtid = async () => {
-        // 참여자 목록을 요청하기 위한 포스트 번호(아이디)
-        const mtid = document.querySelector('input#id').value;
-        // 참여자 목록을 요청할 URL
-        const reqUrl = `/shape/meet/maindetaill/all/${mtid}`;
-        
-        
-        // Ajax 요청을 보내고 응답을 기다림.
-        try {
-            const response = await axios.get(reqUrl);
-            console.log(response);
-            // 댓글 개수 업데이트 & 댓글 목록 보여주기
-            makeReplyElements(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
- * 
- */
+/** 
+ * USER DEtaill 페이지
+ * 유저가 (guest)가 찜 클릭 찜 취소 참여 user정보까지 보여주는 페이지
+**/
 
 	
+	// 찜 클릭
+	const btnLike = document.querySelector('button#like');
+		let isLiked = false; // 초기 상태는 찜 클릭이 아닌 경우
+			btnLike.addEventListener('click', () => {
+	  		if (isLiked) {
+				
+	    	// 이미 찜 완료 상태인 경우
+	    	btnLike.innerHTML = '찜 클릭 ♥';
+	    	isLiked = false;
+	      } else {
+	    	// 찜 완료 상태가 아닌 경우
+	    	btnLike.innerHTML = '찜 완료';
+	    	isLiked = true;	
+	    }
+	});
 	
+	// 참여 클릭
+	const btnjoin = document.querySelector('button#join');
+	let isjoined = false; // 초기 상태는 참여하기가 아닌 경우
+	
+	btnjoin.addEventListener('click', () => {
+		if (isjoined) {
+			// 이미 참여 완료 상태인 경우
+			if (confirm("참여를 취소하시겠습니까?")) {
+				btnjoin.innerHTML = '참여하기';
+				isjoined = false;
+			}
+		} else {
+			// 참여 완료 상태가 아닌 경우
+			if (confirm("참여하시겠습니까?")) {
+				btnjoin.innerHTML = '참여완료';
+				isjoined = true;
+			}
+		}
+	});
+	
+	/**
+	const btnJoin = document.querySelector('#join');
+	let isJoined = false; // 초기 상태는 참여하지 않은 상태
+	
+	btnJoin.addEventListener('click', () => {
+	  if (isJoined) {
+	    // 이미 참여한 상태인 경우
+	    alert('이미 참여하셨습니다.');
+	  } else {
+	    // 참여하지 않은 상태인 경우
+	    // AJAX 요청을 통해 사용자 정보를 가져오고 참여 정보를 삽입합니다.
+	    getUserInfo()
+	      .then((userInfo) => {
+	        if (userInfo) {
+	          // 사용자 정보가 존재하는 경우 참여 정보를 삽입합니다.
+	          insertParticipant(userInfo)
+	            .then(() => {
+	              // 참여 완료 메시지를 표시하고 버튼 상태를 변경합니다.
+	              alert('참여되었습니다.');
+	              btnJoin.innerHTML = '참여완료';
+	              isJoined = true;
+	            })
+	            .catch((error) => {
+	              console.error('참여 정보 삽입 중 오류 발생:', error);
+	            });
+	        } else {
+	          // 사용자 정보가 존재하지 않는 경우
+	          console.error('사용자 정보를 가져올 수 없습니다.');
+	        }
+	      })
+	      .catch((error) => {
+	        console.error('사용자 정보 조회 중 오류 발생:', error);
+	      });
+	  }
+	});
+	
+	// AJAX를 통해 사용자 정보를 가져오는 함수
+	function getUserInfo() {
+	  return new Promise((resolve, reject) => {
+	    const userId = '사용자의 고유한 아이디'; // 사용자의 고유한 아이디 값을 설정해야 합니다.
+	    const xhr = new XMLHttpRequest();
+	    xhr.open('GET', `/getUserInfo?userId=${Id}`);
+	    xhr.onreadystatechange = function () {
+	      if (xhr.readyState === XMLHttpRequest.DONE) {
+	        if (xhr.status === 200) {
+	          const userInfo = JSON.parse(xhr.responseText);
+	          resolve(userInfo);
+	        } else {
+	          reject(xhr.statusText);
+	        }
+	      }
+	    };
+	    xhr.send();
+	  });
+	}
+	
+	// AJAX를 통해 참여 정보를 삽입하는 함수
+	function insertParticipant(userInfo) {
+	  return new Promise((resolve, reject) => {
+	    const participantData = {
+	      mtid: '모임 ID', // 모임 ID 값을 설정해야 합니다.
+	      prtcp_id: userInfo.id,
+	      name: userInfo.name,
+	      gender: userInfo.gender,
+	      phone: userInfo.phone,
+	      email: userInfo.email,
+	    };
+	
+	    const xhr = new XMLHttpRequest();
+	    xhr.open('POST', '/Prtcpinsert');
+	    xhr.setRequestHeader('Content-Type', 'application/json');
+	    xhr.onreadystatechange = function () {
+	      if (xhr.readyState === XMLHttpRequest.DONE) {
+	        if (xhr.status === 200) {
+	          resolve();
+	        } else {
+	          reject(xhr.statusText);
+	        }
+	      }
+	    };
+	    xhr.send(JSON.stringify(participantData));
+	  });
+	}
+
+	 
+	// 참여자 목록
+	
+	document.addEventListener('DOMContentLoaded', () => {
+		// 참여자 목록 표시 (div)
+		const guest = document.querySelector('div#GUEST');
+	
+		// 부트스트랩 Collapse 객체를 생성 - 초기 상태는 화면에서 안보이는 상태
+	    const bsCollapse = new bootstrap.Collapse('div#replyToggleDiv', {toggle: false});
+	    
+	    // 버튼 아이콘 이미지
+	    const toggleBtnIcon = document.querySelector('img#toggleBtnIcon');
+		
+		
+		// 목록 보이기/숨기기 토글 버튼에 이벤트 리스너를 등록
+	    const btnToggleReply = document.querySelector('button#btnToggleReply');
+	    btnToggleReply.addEventListener('click', () => {
+	        bsCollapse.toggle();
+	        
+	        if (toggleBtnIcon.alt === 'toggle-off') {
+	             toggleBtnIcon.src = '../static/images/sample/toggle-on.png';
+	             toggleBtnIcon.alt = 'toggle-on';
+	             
+	             // 댓글 전체 목록을 서버에 요청하고, 응답이 오면 화면 갱신.
+	            // getdetailByMtid();
+	        } else {
+	            toggleBtnIcon.src = '../static/images/sample/toggle-off.png';
+	            toggleBtnIcon.alt = 'toggle-off';
+	            // guest.innerHTML = '';
+	        }
+	    
+	    });
+	});
+	**/
+
+
+
+
 	
 
-	// 부트스트랩 Collapse 객체를 생성 - 초기 상태는 화면에서 안보이는 상태
-    const bsCollapse = new bootstrap.Collapse('div#replyToggleDiv', {toggle: false});
-    
-    // 버튼 아이콘 이미지
-    const toggleBtnIcon = document.querySelector('img#toggleBtnIcon');
 	
-	
-	// 댓글 등록/목록 보이기/숨기기 토글 버튼에 이벤트 리스너를 등록
-    const btnToggleReply = document.querySelector('button#btnToggleReply');
-    btnToggleReply.addEventListener('click', () => {
-        bsCollapse.toggle();
-        
-        if (toggleBtnIcon.alt === 'toggle-off') {
-             toggleBtnIcon.src = '../static/images/sample/toggle-on.png';
-             toggleBtnIcon.alt = 'toggle-on';
-             
-             // 댓글 전체 목록을 서버에 요청하고, 응답이 오면 화면 갱신.
-             getdetailByMtid();
-        } else {
-            toggleBtnIcon.src = '../static/images/sample/toggle-off.png';
-            toggleBtnIcon.alt = 'toggle-off';
-            replies.innerHTML = '';
-        }
-    });
     
     
