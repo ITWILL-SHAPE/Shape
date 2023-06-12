b7657484455e0631c00d08a13e50e5028aecf5d7
woosuvin <dntnqls0605@gmail.com>
2023-06-12 13:47:57 +0900

    댓글 공백 수정

--- src/main/java/com/itwill/shape/dto/PostCommentUpdateDto.java
+++ src/main/java/com/itwill/shape/dto/PostCommentUpdateDto.java
@@ -1,10 +1,16 @@
 package com.itwill.shape.dto;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
+@ToString
 public class PostCommentUpdateDto {
     private String content;
 }
