625cc6af39e198a1aed9096bc005ab5cb5e5324f
itwill <itwill@DESKTOP-T787U4E>
2023-06-16 12:05:10 +0900

    지언 commit main.jsp 수정, style.css 수정

--- src/main/java/com/itwill/shape/web/CommonController.java
+++ src/main/java/com/itwill/shape/web/CommonController.java
@@ -1,5 +1,7 @@
 package com.itwill.shape.web;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -34,9 +36,61 @@ public class CommonController {
 		List<MeetListCountDto> rdto = meetListService.mainReadByRecent();
 		List<MeetListCountDto> pdto = meetListService.mainReadByPopularity();
 		
+		LocalDate currentDate = LocalDate.now();
+
+		List<Boolean> isPastArray = new ArrayList<>(); // 불리언 배열 선언
+		List<LocalDate> targetDate = new ArrayList<>();
+
+		List<MeetListCountDto> filteredDto = new ArrayList<>();
+		for (MeetListCountDto item : rdto) {
+			String date = item.getMt_date();
+			log.info("확인 = {}", date);
+			LocalDate parsedDate = LocalDate.parse(date);
+			targetDate.add(parsedDate);
+			log.info(parsedDate.toString());
+			boolean isPast = currentDate.isBefore(parsedDate); // 현재 시간과 비교하여 지난 시간인지 확인
+
+			if (isPast) { // 모집날짜가 미래일 경우
+				filteredDto.add(item);
+				isPastArray.add(isPast); // 불리언 결과를 리스트에 추가
+
+				
+			} else {
+				log.info("떨구는 것 확인 = {}", item);
+			}
+		}
+		
+		List<MeetListCountDto> filteredDto2 = new ArrayList<>();
+		for (MeetListCountDto item : pdto) {
+			String date = item.getMt_date();
+			log.info("확인 = {}", date);
+			LocalDate parsedDate = LocalDate.parse(date);
+			targetDate.add(parsedDate);
+			log.info(parsedDate.toString());
+			boolean isPast = currentDate.isBefore(parsedDate); // 현재 시간과 비교하여 지난 시간인지 확인
+			
+			if (isPast) { // 모집날짜가 미래일 경우
+				filteredDto2.add(item);
+				isPastArray.add(isPast); // 불리언 결과를 리스트에 추가
+				
+				
+			} else {
+				log.info("떨구는 것 확인 = {}", item);
+			}
+		}
+
+		if (!filteredDto.isEmpty() && !filteredDto2.isEmpty()) {
+			// 뷰에 PostDetailDto를 전달.
+			model.addAttribute("listRecent", filteredDto);
+			model.addAttribute("listPopularity", filteredDto2);
+			
+			for (MeetListCountDto c : filteredDto) {
+				log.info("확인 = {}", c);
+			}
+		} else {
+			log.info("{} 이후로 존재하는 mt_date는 존재하지 않습니다. DB를 확인해보세요...", targetDate.toString());
+		}
 		// 뷰에 PostDetailDto를 전달.
-		model.addAttribute("listRecent", rdto);
-		model.addAttribute("listPopularity", pdto);
 		
 		return "/common/main";
 	}
